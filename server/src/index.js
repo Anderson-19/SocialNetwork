7fe3d6de7843158e8cd8d6958a8028d2d80d3fe5
Anderson-19 <andersonpantor@gmail.com>
2024-06-01 18:30:47 -0400

    Configuración de rutas de los posts y sus interacciones en el index

--- server/src/index.js
+++ server/src/index.js
@@ -6,9 +6,13 @@ dotenv.config();
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/post');
+const postBookmarksRoutes = require('./routes/post-bookmarks');
+const postCommentsRoutes = require('./routes/post-comments');
+const postForwardedRoutes = require('./routes/post-forwarded');
+const postlikesRoutes = require('./routes/post-likes');
 
 const { app, server } = require("./socket/socket.js");
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -23,6 +27,11 @@ app.use(FileUpload({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/post/like", postlikesRoutes);
+app.use("/api/post/forwarded", postForwardedRoutes);
+app.use("/api/post/comment", postCommentsRoutes);
+app.use("/api/post/bookmark", postBookmarksRoutes);
 
 server.listen(PORT, () => {
 	console.log(`Server Running on port ${PORT}`);
