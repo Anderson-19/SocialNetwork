7930bcc760708d17db7fbddc41bbaea218440083
Anderson-19 <andersonpantor@gmail.com>
2024-06-01 17:10:04 -0400

    Función para validar la existencia de un archivo

---
+++ server/src/middlewares/index.js
@@ -0,0 +1,5 @@
+const validateField = require('./validateField');
+const validateFile = require('./validateFile');
+const validateJWT = require('./validateJWT');
+
+module.exports = { validateField, validateFile, validateJWT }
