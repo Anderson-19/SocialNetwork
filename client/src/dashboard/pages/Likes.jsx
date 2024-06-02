0743b7bce5e008c7d80130b2dc42be90f86fd1a6
Anderson-19 <andersonpantor@gmail.com>
2024-06-02 10:56:44 -0400

    Página para ver los likes

---
+++ client/src/dashboard/pages/Likes.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IoArrowBack } from 'react-icons/io5';
+
+import { getLikesByUserId } from '../../api';
+import { useUserStore } from '../../store';
+import { Card } from '../components';
+
+import './styles.css';
+
+export const Likes = () => {
+
+  const [likesByUser, setLikesByUser] = useState([]);
+  const [changeOfInteraction, setChangeOfInteraction] = useState({
+    likes: false,
+    forwarded: false,
+    bookmarks: false
+  });
+  const nav = useNavigate();
+  const user = useUserStore(state => state.user);
+
+  useEffect(() => {
+    getLikesByUserId(user.token, user.uid)
+      .then(res => setLikesByUser(res.likes))
+      .catch(console.log)
+  }, [changeOfInteraction.likes, changeOfInteraction.forwarded, changeOfInteraction.bookmarks]);
+
+  const interactionLikes = () => {
+    setChangeOfInteraction({
+      ...changeOfInteraction,
+      likes: !changeOfInteraction.likes,
+    });
+  }
+
+  const interactionForwarded = () => {
+    setChangeOfInteraction({
+      ...changeOfInteraction,
+      forwarded: !changeOfInteraction.forwarded,
+    });
+  }
+
+  const interactionBookmarks = () => {
+    setChangeOfInteraction({
+      ...changeOfInteraction,
+      bookmarks: !changeOfInteraction.bookmarks,
+    });
+  }
+
+  return (
+    <div className="w-full h-full justify-center overflow-x-hidden overflow-y-scroll scroll-ui bg-white shadow-md border border-y-0 border-gray-800 sm:min-w-fit">
+      <div className="flex">
+        <div className="justify-start px-4 py-2 mx-2 cursor-pointer">
+          <IoArrowBack size={25} onClick={() => nav(localStorage.getItem('pathHome'))} />
+        </div>
+        <div className="flex-1 mx-2">
+          <h2 className="px-4 py-2 text-xl font-semibold text-black">Likes</h2>
+        </div>
+        <div className="flex-1 px-4 py-2 mx-2" />
+      </div>
+
+      <hr className="border-gray-800" />
+
+      {likesByUser &&
+        likesByUser?.map((like) => (
+          <div key={like.post_id} className={`${like.user_id === user.uid ? 'hidden' : ''}`}>
+            <Card
+              key={like.post_id}
+              post={like}
+              interactionLikes={interactionLikes}
+              interactionForwarded={interactionForwarded}
+              interactionBookmarks={interactionBookmarks}
+            />
+          </div>
+        ))
+      }
+    </div>
+  )
+}
