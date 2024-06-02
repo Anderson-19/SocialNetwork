b2d6c29f1a2ece376bb079e92e4c7763205f0750
Anderson-19 <andersonpantor@gmail.com>
2024-06-02 09:55:36 -0400

    Estructura general de la aplicación

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,45 @@
+import { IoAdd } from "react-icons/io5";
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
+import { useUserStore } from './store';
 
+export const App = () => {
 
-function App() {
+  const user = useUserStore(state => state.user);
+
+  const location = useLocation();
+
+  if (!user.isLogged) {
+    return <Navigate to="/" />;
+  }
 
   return (
-    <>
-      <h1 className="text-5xl text-center">Hola Mundo</h1>
-    </>
+    <div className="p-3 h-screen w-full bg-blue-600" >
+      <div className="h-full w-full bg-white overflow-hidden flex flex-col rounded-xl shadow-xl">
+
+      
+        <div className="h-full flex">
+
+          <div className={`${location.pathname === '/messages' ? 'h-full pt-10 px-5' : 'h-full pt-10 px-5 sm:w-0 md:w-1/2 lg:w-1/2 max-[1200px]:w-0'}`}></div>
+         
+
+          <Outlet />
+
+          <div
+            className="absolute px-4 cursor-pointer min-[770px]:hidden" style={{ bottom: '2rem' }}>
+            <div className="flex bg-blue-500 hover:bg-blue-400 rounded-full px-3 py-3 mt-12 mr-2">
+              <IoAdd size={45} color="white" />
+            </div>
+          </div>
+
+         
+          <div className={`${location.pathname === '/messages' ? 'hidden' : 'h-full w-screen border-r pt-8 px-5 max-[1200px]:w-0'}`}>
+           
+          </div>
+
+
+        </div>
+      </div>
+    </div >
   )
 }
 
-export default App
